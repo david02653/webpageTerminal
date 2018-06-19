27c6e8377bd3f88bc3b70454df08ccdff109c84e
david02653 <david02653@gmail.com>
2018-06-19 23:17:12 +0800

    update
    nothing special

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-$(document).ready(function(){
-    console.log("jquery");
-    $("#icon").hover(function(){
-        console.log("hover");
-        $("#icon").removeClass("animated bounceIn");
-        $("#icon").addClass("animated infinite bounceIn");
-    },
-    function(){
-        $("#icon").removeClass("animated infinite bounceIn");
-        $("#icon").addClass("animated bounceIn");
-    });
-});
+var target;
+
+function start()
+{
+    
+}
+
+function titleHover(event)
+{
+
+}
+
+window.addEventListener("load", start, false);
